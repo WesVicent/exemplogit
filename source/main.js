0409421d54e3f9fb1ebb424c4763914b46b03801
Wesley Vicente <wesley.soften@gmail.com>
2024-04-27 01:41:11 -0300

    Bouncing ball adicionado

--- source/main.js
+++ source/main.js
@@ -1,7 +1,73 @@
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("1");
-});
 
-window.onload = function () {
-    console.log("2");
+var onLoad = function (event) {
+  // Key listener.
+  window.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      isRunning = !isRunning;
+
+      if (isRunning) {
+        window.requestAnimationFrame(draw);
+      }
+    }
+  });
+
+  var canvas = document.createElement("canvas");
+  canvas.id = "screen";
+  canvas.width = 500;
+  canvas.height = 500;
+  canvas.style.border = "1px solid #CCCCCC";
+
+  const container = document.querySelector("#container");
+  container.appendChild(canvas);
+
+  const screen = document.getElementById("screen");
+
+  if (screen.getContext) {
+    var isRunning = true;
+    var context = screen.getContext("2d");
+
+    var reverse = false;
+    x = 50;
+    y = 50;
+
+    function draw() {
+      if (!isRunning) {
+        return;
+      }
+
+      window.requestAnimationFrame(draw);
+
+      context.save();
+      context.clearRect(0, 0, 500, 500);
+
+      context.beginPath();
+      context.arc(x, y, 50, 0, 2 * Math.PI);
+      context.fillStyle = "#1180ff";
+      context.fill();
+
+      if (reverse) {
+        x -= 5;
+        y -= 5;
+      } else {
+        x += 5;
+        y += 5;
+      }
+
+      if (y >= 450) {
+        reverse = true;
+      }
+
+      if (y <= 50) {
+        reverse = false;
+      }
+
+      context.restore();
+    }
+
+    window.requestAnimationFrame(draw);
+  } else {
+    window.alert("O navegador não suporta o recurso 'Canvas' do HTML5 ");
+  }
 };
+
+window.addEventListener("load", onLoad);
